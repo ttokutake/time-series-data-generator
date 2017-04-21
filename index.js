51984edaf2402c89fe9734514953638eb7b9c938
aTadatoshiTokutake <tadatoshi.tokutake@access-company.com>
2017-04-21 21:32:29 +0900

    Validate args of dummyAccessLog()

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const {dummyAccessLog} = require('./lib/dummy/access_log');
 
 seed(1);
 
-const numOfUsers    = 100;
 const numOfLines    = 10;
+const numOfUsers    = 100;
 const keysOfCrudReq = ['products', 'comments'];
-const accessLog     = dummyAccessLog({numOfUsers, numOfLines, keysOfCrudReq});
+const accessLog     = dummyAccessLog({numOfLines, numOfUsers, keysOfCrudReq});
 
 console.log(JSON.stringify(accessLog));
