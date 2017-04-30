3bf53c972c6c2d7d22bac99d8442d5080c18f5ff
ttokutake <tadatoshi.tokutake@gmail.com>
2017-04-30 17:25:05 +0900

    Add test for CrudReqGenerator.validate()

--- test/dummy/req_generator.test.js
+++ test/dummy/req_generator.test.js
@@ -1,4 +1,5 @@
 const {
+  CrudReqGenerator,
   ReqGenerator,
 } = require('../../lib/dummy/req_generator');
 
@@ -55,24 +56,28 @@ describe('ReqGenerator', () => {
       [reqParamBase.set('method', 0        ).toJSON()],
       [reqParamBase.set('method', []       ).toJSON()],
       [reqParamBase.set('method', {}       ).toJSON()],
+      [reqParamBase.delete('method')        .toJSON()],
 
       [reqParamBase.set('path', undefined).toJSON()],
       [reqParamBase.set('path', null     ).toJSON()],
       [reqParamBase.set('path', 0        ).toJSON()],
       [reqParamBase.set('path', []       ).toJSON()],
       [reqParamBase.set('path', {}       ).toJSON()],
+      [reqParamBase.delete('path')        .toJSON()],
 
       [reqParamBase.set('ratio', undefined).toJSON()],
       [reqParamBase.set('ratio', null     ).toJSON()],
       [reqParamBase.set('ratio', '0'      ).toJSON()],
       [reqParamBase.set('ratio', []       ).toJSON()],
       [reqParamBase.set('ratio', {}       ).toJSON()],
+      [reqParamBase.delete('ratio')        .toJSON()],
 
       [reqParamBase.set('statusRatio', undefined).toJSON()],
       [reqParamBase.set('statusRatio', null     ).toJSON()],
       [reqParamBase.set('statusRatio', 0        ).toJSON()],
       [reqParamBase.set('statusRatio', '0'      ).toJSON()],
       [reqParamBase.set('statusRatio', []       ).toJSON()],
+      [reqParamBase.delete('statusRatio')        .toJSON()],
     ];
 
     for (const input of inputs) {
@@ -117,5 +122,99 @@ describe('ReqGenerator', () => {
     const input = [reqParamBase.set('statusRatio', {}).toJSON()];
 
     expect(new ReqGenerator(input).randomRequest()).toEqual({method: 'HEAD', path: '/index.html', status: '-'});
+  });
+});
+
+describe('CrudReqGenerator', () => {
+
+  const reqParamBase = Map({
+    resource   : 'users',
+    methodRatio: Map({
+      POST  : Map({ratio: 1, statusRatio: {201: 1}}),
+      GET   : Map({ratio: 1, statusRatio: {200: 1}}),
+      PUT   : Map({ratio: 1, statusRatio: {200: 1}}),
+      DELETE: Map({ratio: 1, statusRatio: {204: 1}}),
+    }),
+  });
+
+  test('constructor() should throw Error', () => {
+    const inputs = [
+      undefined,
+      null,
+      0,
+      '0',
+      [],
+
+      reqParamBase.set('resource', undefined).toJS(),
+      reqParamBase.set('resource', null     ).toJS(),
+      reqParamBase.set('resource', 0        ).toJS(),
+      reqParamBase.set('resource', []       ).toJS(),
+      reqParamBase.set('resource', {}       ).toJS(),
+      reqParamBase.delete('resource').toJS(),
+
+      reqParamBase.set('methodRatio', undefined ).toJS(),
+      reqParamBase.set('methodRatio', null      ).toJS(),
+      reqParamBase.set('methodRatio', 0         ).toJS(),
+      reqParamBase.set('methodRatio', '0'       ).toJS(),
+      reqParamBase.set('methodRatio', []        ).toJS(),
+      reqParamBase.set('methodRatio', {HEAD: {}}).toJS(),
+      reqParamBase.delete('methodRatio').toJS(),
+
+      reqParamBase.setIn(['methodRatio', 'POST'  , 'ratio'], undefined).toJS(),
+      reqParamBase.setIn(['methodRatio', 'POST'  , 'ratio'], null     ).toJS(),
+      reqParamBase.setIn(['methodRatio', 'POST'  , 'ratio'], '0'      ).toJS(),
+      reqParamBase.setIn(['methodRatio', 'POST'  , 'ratio'], []       ).toJS(),
+      reqParamBase.setIn(['methodRatio', 'POST'  , 'ratio'], {}       ).toJS(),
+      reqParamBase.setIn(['methodRatio', 'GET'   , 'ratio'], undefined).toJS(),
+      reqParamBase.setIn(['methodRatio', 'GET'   , 'ratio'], null     ).toJS(),
+      reqParamBase.setIn(['methodRatio', 'GET'   , 'ratio'], '0'      ).toJS(),
+      reqParamBase.setIn(['methodRatio', 'GET'   , 'ratio'], []       ).toJS(),
+      reqParamBase.setIn(['methodRatio', 'GET'   , 'ratio'], {}       ).toJS(),
+      reqParamBase.setIn(['methodRatio', 'PUT'   , 'ratio'], undefined).toJS(),
+      reqParamBase.setIn(['methodRatio', 'PUT'   , 'ratio'], null     ).toJS(),
+      reqParamBase.setIn(['methodRatio', 'PUT'   , 'ratio'], '0'      ).toJS(),
+      reqParamBase.setIn(['methodRatio', 'PUT'   , 'ratio'], []       ).toJS(),
+      reqParamBase.setIn(['methodRatio', 'PUT'   , 'ratio'], {}       ).toJS(),
+      reqParamBase.setIn(['methodRatio', 'DELETE', 'ratio'], undefined).toJS(),
+      reqParamBase.setIn(['methodRatio', 'DELETE', 'ratio'], null     ).toJS(),
+      reqParamBase.setIn(['methodRatio', 'DELETE', 'ratio'], '0'      ).toJS(),
+      reqParamBase.setIn(['methodRatio', 'DELETE', 'ratio'], []       ).toJS(),
+      reqParamBase.setIn(['methodRatio', 'DELETE', 'ratio'], {}       ).toJS(),
+
+      reqParamBase.deleteIn(['methodRatio', 'POST'  , 'ratio']).toJS(),
+      reqParamBase.deleteIn(['methodRatio', 'GET'   , 'ratio']).toJS(),
+      reqParamBase.deleteIn(['methodRatio', 'PUT'   , 'ratio']).toJS(),
+      reqParamBase.deleteIn(['methodRatio', 'DELETE', 'ratio']).toJS(),
+
+      reqParamBase.setIn(['methodRatio', 'POST'  , 'statusRatio'], undefined).toJS(),
+      reqParamBase.setIn(['methodRatio', 'POST'  , 'statusRatio'], null     ).toJS(),
+      reqParamBase.setIn(['methodRatio', 'POST'  , 'statusRatio'], 0        ).toJS(),
+      reqParamBase.setIn(['methodRatio', 'POST'  , 'statusRatio'], '0'      ).toJS(),
+      reqParamBase.setIn(['methodRatio', 'POST'  , 'statusRatio'], []       ).toJS(),
+      reqParamBase.setIn(['methodRatio', 'GET'   , 'statusRatio'], undefined).toJS(),
+      reqParamBase.setIn(['methodRatio', 'GET'   , 'statusRatio'], null     ).toJS(),
+      reqParamBase.setIn(['methodRatio', 'GET'   , 'statusRatio'], 0        ).toJS(),
+      reqParamBase.setIn(['methodRatio', 'GET'   , 'statusRatio'], '0'      ).toJS(),
+      reqParamBase.setIn(['methodRatio', 'GET'   , 'statusRatio'], []       ).toJS(),
+      reqParamBase.setIn(['methodRatio', 'PUT'   , 'statusRatio'], undefined).toJS(),
+      reqParamBase.setIn(['methodRatio', 'PUT'   , 'statusRatio'], null     ).toJS(),
+      reqParamBase.setIn(['methodRatio', 'PUT'   , 'statusRatio'], 0        ).toJS(),
+      reqParamBase.setIn(['methodRatio', 'PUT'   , 'statusRatio'], '0'      ).toJS(),
+      reqParamBase.setIn(['methodRatio', 'PUT'   , 'statusRatio'], []       ).toJS(),
+      reqParamBase.setIn(['methodRatio', 'DELETE', 'statusRatio'], undefined).toJS(),
+      reqParamBase.setIn(['methodRatio', 'DELETE', 'statusRatio'], null     ).toJS(),
+      reqParamBase.setIn(['methodRatio', 'DELETE', 'statusRatio'], 0        ).toJS(),
+      reqParamBase.setIn(['methodRatio', 'DELETE', 'statusRatio'], '0'      ).toJS(),
+      reqParamBase.setIn(['methodRatio', 'DELETE', 'statusRatio'], []       ).toJS(),
+
+      reqParamBase.deleteIn(['methodRatio', 'POST'  , 'statusRatio']).toJS(),
+      reqParamBase.deleteIn(['methodRatio', 'GET'   , 'statusRatio']).toJS(),
+      reqParamBase.deleteIn(['methodRatio', 'PUT'   , 'statusRatio']).toJS(),
+      reqParamBase.deleteIn(['methodRatio', 'DELETE', 'statusRatio']).toJS(),
+    ];
+
+    for (const input of inputs) {
+      expect(() => new CrudReqGenerator(input)).toThrow();
+    }
   });
 });
