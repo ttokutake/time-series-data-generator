75fa463504ec8d472d9f0e403298274566770151
aTadatoshiTokutake <tadatoshi.tokutake@access-company.com>
2017-05-02 19:17:16 +0900

    Add tests for DateGenerator

---
+++ test/dummy/date_generator.test.js
@@ -0,0 +1,109 @@
+const DateGenerator = require('../../lib/dummy/date_generator');
+
+const is       = require('is_js');
+const jsc      = require('jsverify');
+const mockDate = require('mockdate');
+const moment   = require('moment');
+
+describe('DateGenerator', () => {
+  afterEach(() => {
+    mockDate.reset();
+  });
+
+  test('constructor() should throw Error', () => {
+    const now    = moment();
+    const inputs = [
+      false,
+      0,
+      0.1,
+      '',
+      [],
+      {key: null      },
+      {key: new Date()},
+
+      {from: false},
+      {from: 0.1  },
+      {from: ''   },
+      {from: []   },
+      {from: {}   },
+
+      {until: false},
+      {until: 0.1  },
+      {until: ''   },
+      {until: []   },
+      {until: {}   },
+
+      {from: now.toDate(), until: now.subtract(1, 'milliseconds').toDate()}
+    ];
+
+    for (const input of inputs) {
+      expect(() => new DateGenerator(input)).toThrow();
+    }
+  });
+
+  test('randomDate() should return recent Date', () => {
+    jsc.assertForall(jsc.elements([undefined, null]), (input) => {
+      const output = moment(new DateGenerator(input).randomDate());
+      const now    = new Date();
+      const from   = moment(now).subtract(1, 'days');
+      const until  = now;
+      return output.isBetween(from, now, null, '[)');
+    });
+  });
+
+  test('randomDate() should return Date satisfying "from" <= "until"', () => {
+    jsc.assertForall(jsc.datetime, jsc.datetime, (x, y) => {
+      const [from, until] = x.getTime() > y.getTime() ? [y, x] : [x, y];
+
+      const inputs = [
+        {from                    , until                     },
+        {from: from.getTime()    , until: until.getTime()    },
+        {from: from.toISOString(), until: until.toISOString()},
+      ];
+      for (const input of inputs) {
+        const outputDate = moment(new DateGenerator(input).randomDate());
+        expect(outputDate.isBetween(from, until, null, '[]')).toBeTruthy();
+      }
+
+      return true;
+    });
+  });
+
+  test('randomDate() should return Date satisfying "from" <= now', () => {
+    mockDate.set('2100-01-01T00:00:00.000Z');
+    jsc.assertForall(jsc.datetime, (from) => {
+      const now = new Date();
+
+      const inputs = [
+        {from                  },
+        {from, until: undefined},
+        {from, until: null     },
+      ];
+      for (const input of inputs) {
+        const output = moment(new DateGenerator(input).randomDate());
+        expect(output.isBetween(from, now, null, '[]')).toBeTruthy();
+      }
+
+      return true;
+    });
+  });
+
+  test('randomDate() should return Date satisfying now <= "until"', () => {
+    mockDate.set('2000-01-01T00:00:00.000Z');
+    jsc.assertForall(jsc.datetime, (until) => {
+      const now = new Date();
+
+      const inputs = [
+        {                 until},
+        {from: undefined, until},
+        {from: null     , until},
+      ];
+      for (const input of inputs) {
+        const output = moment(new DateGenerator(input).randomDate());
+        expect(output.isBetween(now, until, null, '[]')).toBeTruthy();
+      }
+
+      return true;
+    });
+  });
+});
