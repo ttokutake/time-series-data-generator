852f070702c767e920e9828fdd324f796080153b
ttokutake <tadatoshi.tokutake@gmail.com>
2017-05-06 14:18:51 +0900

    Use describe()

--- test/random.test.js
+++ test/random.test.js
@@ -5,36 +5,38 @@ const {
 const is  = require('is_js');
 const jsc = require('jsverify');
 
-test('randomInt() should return integer between range of min and max', () => {
-  jsc.assertForall(jsc.integer(), jsc.integer(), (x, y) => {
-    const [min, max] = x < y ? [x, y] : [y, x];
-    const result     = randomInt(min, max);
-    return min <= result && result <= max;
+describe('randomInt()', () => {
+  test('it should return integer between range of min and max', () => {
+    jsc.assertForall(jsc.integer(), jsc.integer(), (x, y) => {
+      const [min, max] = x < y ? [x, y] : [y, x];
+      const result     = randomInt(min, max);
+      return min <= result && result <= max;
+    });
   });
-});
 
-test('randomInt() should return null', () => {
-  jsc.assertForall(jsc.integer(), jsc.integer(), (x, y) => {
-    if (x === y) {
-      return true;
+  test('it should return null', () => {
+    jsc.assertForall(jsc.integer(), jsc.integer(), (x, y) => {
+      if (x === y) {
+        return true;
+      }
+      const [min, max] = x < y ? [y, x] : [x, y];
+      const result     = randomInt(min, max);
+      return is.null(result);
+    });
+  });
+
+  test('it should throw Error', () => {
+    const inputs = [
+      undefined,
+      null,
+      '0',
+      [],
+      {},
+    ];
+
+    for (input of inputs) {
+      expect(() => randomInt(input, 0    )).toThrow(/min/);
+      expect(() => randomInt(0    , input)).toThrow(/max/);
     }
-    const [min, max] = x < y ? [y, x] : [x, y];
-    const result     = randomInt(min, max);
-    return is.null(result);
   });
-});
-
-test('randomInt() should throw Error', () => {
-  const inputs = [
-    undefined,
-    null,
-    '0',
-    [],
-    {},
-  ];
-
-  for (input of inputs) {
-    expect(() => randomInt(input, 0    )).toThrow(/min/);
-    expect(() => randomInt(0    , input)).toThrow(/max/);
-  }
 });
--- test/type.test.js
+++ test/type.test.js
@@ -4,31 +4,33 @@ const {
 } = require('../lib/type');
 
 
-class Class {
-}
-class ExtendedClass extends Class {
-}
-
-test('isInstanceOf() should return true and isNotInstanceOf() should do false', () => {
-  const data = [
-    [new Class()        , Class        ],
-    [new ExtendedClass(), Class        ],
-    [new ExtendedClass(), ExtendedClass],
-  ];
-
-  for (const [input, cls] of data) {
-    expect(isInstanceOf(input, cls)   ).toBeTruthy();
-    expect(isNotInstanceOf(input, cls)).toBeFalsy();
+describe('isInstanceOf() & isNotInstanceOf()', () => {
+  class Class {
   }
-});
-
-test('isInstanceOf() should return false and isNotInstanceOf() should do true', () => {
-  const data = [
-    [new Class(), ExtendedClass],
-  ];
-
-  for (const [input, cls] of data) {
-    expect(isInstanceOf(input, cls)   ).toBeFalsy();
-    expect(isNotInstanceOf(input, cls)).toBeTruthy();
+  class ExtendedClass extends Class {
   }
+
+  test('it should return true & opposite should do false', () => {
+    const data = [
+      [new Class()        , Class        ],
+      [new ExtendedClass(), Class        ],
+      [new ExtendedClass(), ExtendedClass],
+    ];
+
+    for (const [input, cls] of data) {
+      expect(isInstanceOf(input, cls)   ).toBeTruthy();
+      expect(isNotInstanceOf(input, cls)).toBeFalsy();
+    }
+  });
+
+  test('it should return false & opposite should do true', () => {
+    const data = [
+      [new Class(), ExtendedClass],
+    ];
+
+    for (const [input, cls] of data) {
+      expect(isInstanceOf(input, cls)   ).toBeFalsy();
+      expect(isNotInstanceOf(input, cls)).toBeTruthy();
+    }
+  });
 });
