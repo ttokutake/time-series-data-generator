017ff6c8bc290d15a22e4978795edf3ceeb97002
aTadatoshiTokutake <tadatoshi.tokutake@access-company.com>
2017-05-25 09:18:47 +0900

    Add Series.ratio()

--- index.js
+++ index.js
@@ -5,10 +5,18 @@ const until    = '2016-01-01T01:00:00Z';
 const interval = 5 * 60; // seconds
 const series = new Series({from, until, interval});
 
-var coefficient = 1;
-var constant    = 0;
-var period      = 1 * 60 * 60; // seconds
+const coefficient = 1;
+const constant    = 0;
+const period      = 1 * 60 * 60; // seconds
 const series_cos = series.cos({coefficient, constant, period});
 console.log(series_cos);
 const series_sin = series.sin({coefficient, constant, period});
 console.log(series_sin);
+
+const params = {
+  rock    : 2,
+  scissors: 2,
+  paper   : 1,
+};
+const series_ratio = series.ratio(params);
+console.log(series_ratio);
