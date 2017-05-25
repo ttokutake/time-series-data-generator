1630284a91d7259b1cc492fc4d4ba9301c5cedb2
aTadatoshiTokutake <tadatoshi.tokutake@access-company.com>
2017-05-25 10:27:58 +0900

    Add options.decimalDigits

--- index.js
+++ index.js
@@ -5,12 +5,13 @@ const until    = '2016-01-01T01:00:00Z';
 const interval = 5 * 60; // seconds
 const series = new Series({from, until, interval});
 
-const coefficient = 1;
-const constant    = 0;
-const period      = 1 * 60 * 60; // seconds
-const series_cos = series.cos({keyName: 'alpha', coefficient, constant, period});
+const coefficient   = 1;
+const constant      = 0;
+const decimalDigits = 3;
+const period        = 1 * 60 * 60; // seconds
+const series_cos = series.cos({keyName: 'alpha', coefficient, constant, decimalDigits, period});
 console.log(series_cos);
-const series_sin = series.sin({keyName: 'beta', coefficient, constant, period});
+const series_sin = series.sin({keyName: 'beta', coefficient, constant, decimalDigits, period});
 console.log(series_sin);
 
 const params = {
