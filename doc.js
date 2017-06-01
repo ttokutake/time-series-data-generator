ce8605f5e2850ed58a115a030005a5160e0a554f
ttokutake <tadatoshi.tokutake@gmail.com>
2017-06-01 21:37:52 +0900

    Create gh-pages

--- doc.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const app = express();
-
-app.use('/', express.static('out'));
-
-app.listen(3000, () => {
-  console.log('Document server listening on port 3000!');
-})
--- test/util.js
+++
@@ -1,9 +0,0 @@
-const jsc = require('jsverify');
-
-const jscPosInteger    = jsc.nat.smap((x) => x + 1, (x) => x - 1);
-const jscNonPosInteger = jsc.nat.smap((x) => -x   , (x) => -x   );
-
-module.exports = {
-  jscPosInteger,
-  jscNonPosInteger,
-}
